89a29b5aae020e06a6754ac7088e32e57d741af6
adapoeyy <72613600+adapoeyy@users.noreply.github.com>
2020-11-17 00:17:41 +0700

    Add files via upload

---
+++ 1-Login Authentication/otentikasi.js
@@ -0,0 +1,13 @@
+
+function login(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "ahmad2017" && password == "integrity"){
+alert ("Login successsfully");
+window.location = "index.html";
+  }
+  else{
+    alert("Invalid username or password");
+    }
+  return false;
+  }
